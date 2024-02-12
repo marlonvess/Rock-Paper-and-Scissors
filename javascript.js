3cc127ba29d98f8665ad1af20a2e8e1ce648ed4e
marlonvess <marlonvess@gmail.com>
2024-02-12 23:15:18 +0000

    score and choices visible

--- javascript.js
+++ javascript.js
@@ -1,24 +1,37 @@
+//Array to select a number and output game results
 let choices=['rock','paper','scissors'];
 
+let playerPoints=0;
+let computerPoints=0;
 
 function playGame(playerChoice) {
-    
+    //Variable to randomly choose a number from 0 to 2 inside an array
     let cpuChoices=choices[Math.floor(Math.random()*3)]
+    
+    //Output Player and Computer Choices
+    document.getElementById("playerChoice").innerHTML = "Player choice:"+playerChoice;
+    document.getElementById("computerChoice").innerHTML="Computer choice:"+cpuChoices;
 
+    //Make comparation between Player choices and computer choices to output results to player
     if (playerChoice==cpuChoices) {
-        console.log("Tie");
+        document.getElementById("result").innerHTML="Tie!";
     } 
     else if (playerChoice=='rock' && cpuChoices=='scissors' || 
              playerChoice=='paper' && cpuChoices=='rock' ||
              playerChoice=='scissors' && cpuChoices=='paper'   
-                                        )                      
-                                    {
-        console.log("You win");
+            )                      
+     {
+        document.getElementById("result").innerHTML="You Win!";
+        playerPoints++;
     }
     else {
-        console.log ("You lose!");
+        document.getElementById("result").innerHTML="You Lose!";
+        computerPoints++;
     }
-    console.log (choices)
-    console.log (playerChoice);
-    console.log (cpuChoices);
+    
+    document.getElementById("playerPoints").innerHTML="Player Points:"+playerPoints;
+    document.getElementById("computerPoints").innerHTML="Computer Points:"+computerPoints;
+    
 }
+
+
